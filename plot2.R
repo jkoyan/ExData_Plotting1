9b1ff03c1600df59fa998cdd79c30a512d9d29aa
Jimmy Koyan <jimmy.koyan@gmail.com>
2015-03-08 11:09:45 -0400

    Added to code to create the Plot2.png file

--- plot2.R
+++ plot2.R
@@ -10,11 +10,18 @@ createPlot2 <- function(workingdir="C:\\_R\\Exploratory Data Analysis\\Course Pr
     l <- (data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
     data <- data[which(l),]
     data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
-    
-    data <- mutate(data,Day=wday(data$Date,label=TRUE))
-    #plot(d$Day,d$Global_active_power,ylab="Gloabl active power (kilowatts)"type="s")
-    #png(file = "plot1.png",width = 480, height = 480, bg = "white")
-    #hist(data$Global_active_power,xlab="Gloabl active power (kilowatts)",main="Global Active Power",col="red")
-    #dev.off()
+    dateTimes <- paste(as.character(data$Date),data$Time,sep=" ")
+    data <- mutate(data,Day=wday(data$Date,label=TRUE),DateTime=ymd_hms(paste(as.character(data$Date),data$Time,sep=" ")))
+    data$Day <- gsub("^Thurs","Thu",data$Day)
+    data <-group_by(data,Day)
+    data
+    if(file.exists("plot2.png"))
+    {
+        print("The file plot2.png already exists in the working directory, deleting the file before creating a new plot.")
+        unlink("plot2.png")
+    }
+    png(file = "plot2.png",width = 480, height = 480, bg = "white")
+    plot(data$DateTime,data$Global_active_power,xlab=" ",ylab="Global active power (kilowatts)",type="l")
+    dev.off()
     
 }
